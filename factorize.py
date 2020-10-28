808db5ee63bffc77bc2a97dedfecfbcfa085c22f
luca-serra <luca.serra@student.ecp.fr>
2020-10-28 18:59:32 +0100

    Wheel factorization

--- factorize.py
+++ factorize.py
@@ -36,7 +36,7 @@ def factorize(numbers):
         """Return the smallest prime number greater than number"""
         n = number + 1
         prime = False
-        while not(prime):
+        while not (prime):
             prime = True
             for p in range(2, int(ma.sqrt(n)) + 1):
                 if n % p == 0:
@@ -47,86 +47,97 @@ def factorize(numbers):
             prime = False
             n += 1
 
-    def _factorize(number, factors):
-        """Return the prime factorization of number as a list"""
-        factors = copy.copy(factors)
+    # def _factorize(number, factors):
+    #     """Return the prime factorization of number as a list"""
+    #     factors = copy.copy(factors)
+    #     factors_number = []
+    #     d = 2
+    #     if len(factors) == 0:
+    #         factors.append(d)
+    #     idx_d = 0
+    #     numb = number  # copy to keep number as it is
+    #     while (numb != 1):
+    #         while numb % d == 0:
+    #             factors_number.append(d)
+    #             numb = numb // d
+    #         if len(factors_number) == 0 and d > ma.sqrt(number):
+    #             return ([number], factors)
+    #         if len(factors) > 0 and idx_d < len(factors) - 1:
+    #             idx_d += 1
+    #             d = factors[idx_d]
+    #         else:
+    #             d = _get_next_prime(d)
+    #             factors.append(d)
+
+    #     return (factors_number, factors)
+    def _factorize_wheel(number):
         factors_number = []
-        d = 2
-        if len(factors) == 0:
-            factors.append(d)
-        idx_d = 0
-        numb = number  # copy to keep number as it is
-        while (numb != 1):
-            while numb % d == 0:
+        base = [2, 3, 5]
+        n = number
+        for d in base:
+            while n % d == 0:
                 factors_number.append(d)
-                numb = numb // d
-            if len(factors_number) == 0 and d > ma.sqrt(number):
-                return ([number], factors)
-            if len(factors) > 0 and idx_d < len(factors) - 1:
-                idx_d += 1
-                d = factors[idx_d]
-            else:
-                d = _get_next_prime(d)
-                factors.append(d)
+                n = n // d
 
-        return (factors_number, factors)
+        increments = [4, 2, 4, 2, 4, 6, 2, 6]
+        i = 0
+        d = 7
+        while d <= ma.sqrt(n):
+            while n % d == 0:
+                factors_number.append(d)
+                n = n // d
+            d += increments[i]
+            i += 1
+            if i == 8:  # 8 is the length of the increments cycle
+                i = 0
+        if n > 1:
+            factors_number.append(n)
+        return factors_number
 
-    # max_number = max(numbers)
-    # factors = _get_prime_numbers(int(ma.sqrt(max_number)) + 1)
     result = {}
-    list_primes = [2]
-    numbers = sorted(numbers)
-    print(numbers)
-    # print(numbers)
     for i, number in enumerate(numbers):
-        # if i == 0 or i==1 or i==3:
-        #     print(i)
         # print(number)
-        # print('before', list_primes)
-        res = _factorize(number, list_primes)
-        list_primes = res[1]
-        # print('after', list_primes)
-        result[number] = res[0]
+        result[number] = _factorize_wheel(number)
         # result[number] = _factorize(number, list_primes)
 
     return result
 
+
 #########################################
 #### Ne pas modifier le code suivant ####
 #########################################
-if __name__=="__main__":
+if __name__ == "__main__":
     input_dir = os.path.abspath(sys.argv[1])
     output_dir = os.path.abspath(sys.argv[2])
-    
+
     # un repertoire des fichiers en entree doit être passé en parametre 1
     if not os.path.isdir(input_dir):
-	    print(input_dir, "doesn't exist")
-	    exit()
+        print(input_dir, "doesn't exist")
+        exit()
 
     # un repertoire pour enregistrer les résultats doit être passé en parametre 2
     if not os.path.isdir(output_dir):
-	    print(output_dir, "doesn't exist")
-	    exit()       
+        print(output_dir, "doesn't exist")
+        exit()
 
-     # Pour chacun des fichiers en entrée 
+        #  Pour chacun des fichiers en entrée
     for data_filename in sorted(os.listdir(input_dir)):
         # if data_filename == 'training_data01.txt':
         print(data_filename)
-        # importer la liste des nombres
+        #  importer la liste des nombres
         data_file = open(os.path.join(input_dir, data_filename), "r")
-        numbers = [int(line) for line in data_file.readlines()]        
-        
+        numbers = [int(line) for line in data_file.readlines()]
+
         # decomposition en facteurs premiers
         D = factorize(numbers)
 
         # fichier des reponses depose dans le output_dir
-        output_filename = 'answer_{}'.format(data_filename)             
+        output_filename = 'answer_{}'.format(data_filename)
         output_file = open(os.path.join(output_dir, output_filename), 'w')
-        
+
         # ecriture des resultats
         for (n, primes) in D.items():
             output_file.write('{} {}\n'.format(n, primes))
-        
+
         output_file.close()
 
-    
